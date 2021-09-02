6f16be2df39e2cf1e9cc9ea156710a92bdcb51b0
Mohammad Abdul Ahad <maahad.767@gmail.com>
2021-09-02 18:02:09 +0600

    implemented token based authentication with simple jwt

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # project local app
+    'account',
+    'product',
+
+    # 3rd party apps
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +131,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'account.User'
+
+# django rest framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
---
+++ product/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+
+app_name = 'product'
+
+urlpatterns = [
+    path('api/', include('product.api.urls')),
+]
